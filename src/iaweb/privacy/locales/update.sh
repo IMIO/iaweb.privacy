c1145b602c5365a9b2c422b2adc26fef8f82d205
Martin Peeters <martin.peeters@affinitic.be>
2020-04-30 16:44:29 +0200

    WEB-3260: Update translation files

--- src/iaweb/privacy/locales/update.sh
+++ src/iaweb/privacy/locales/update.sh
@@ -10,3 +10,8 @@ domain=iaweb.privacy
 
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+domain=collective.privacy
+
+i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
