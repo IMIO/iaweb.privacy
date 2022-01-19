f3522c153d5b85d8389c9e8734ddda3d78a87bfe
Laurent Lasudry <lasudry@gmail.com>
2022-01-19 13:55:22 +0100

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="iaweb.privacy",
-    version="1.0a2",
+    version="1.0a3.dev0",
     description="Privacy rules for IMIO web projects",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
